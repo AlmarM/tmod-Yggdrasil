ccecad99228200268ad9ef97888308fb1e24b63c
Jeff-resP <jeffgagnenord@gmail.com>
2022-05-04 21:23:20 +0200

    Added runic power to Flesh Runic Axe. Removed a line in ObsidianHammer and remove axe power in vikingsword. Added Viking and FrostCore spear item and projectiles

---
+++ Content/Items/Weapons/FrostCore/FrostCoreSpear.cs
@@ -0,0 +1,48 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using Yggdrasil.Content.Projectiles;
+using Yggdrasil.Content.Items.Materials;
+
+namespace Yggdrasil.Content.Items.Weapons.FrostCore
+{
+	public class FrostCoreSpear : YggdrasilItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("FrostCore Spear");
+			Tooltip.SetDefault("50% chance to frostburn target for 3 sec");
+
+			ItemID.Sets.SkipsInitialUseSound[Item.type] = true; // This skips use animation-tied sound playback, so that we're able to make it be tied to use time instead in the UseItem() hook.
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		public override void SetDefaults() 
+		{
+			Item.damage = 18;
+			Item.useTime = 23;
+			Item.useAnimation = 23;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.knockBack = 4;
+			Item.crit = 0;
+			Item.noUseGraphic = true;
+			Item.DamageType = DamageClass.Melee;
+			Item.noMelee = true;
+			Item.shootSpeed = 3.5f;
+			Item.shoot = ModContent.ProjectileType<FrostCoreSpearProjectile>();
+
+			Item.value = Item.sellPrice(0, 0, 23, 0);
+			Item.rare = ItemRarityID.Orange;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = false;
+
+		}
+
+		public override void AddRecipes() => CreateRecipe()
+		.AddIngredient<FrostCoreBar>(8)
+		.AddTile(TileID.Anvils)
+		.Register();
+
+	}
+}
