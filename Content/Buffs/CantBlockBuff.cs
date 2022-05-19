982d39b9e9f98bd2191b6159f2c2a6c938d6da71
Jeff-resP <jeffgagnenord@gmail.com>
2022-05-19 21:10:30 +0200

    Added a new Woodlance for the new weapon class. Also added a Block and cantblock buff and a defensive stance buff.

---
+++ Content/Buffs/CantBlockBuff.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Yggdrasil.Content.Players;
+using Yggdrasil.Utils;
+
+namespace Yggdrasil.Content.Buffs;
+
+public class CantBlockBuff : YggdrasilBuff
+{
+    public override string Texture => TextureUtils.GetAssetPath(GetType());
+
+    public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Block Cooldown");
+        Description.SetDefault("");
+
+        Main.buffNoSave[Type] = true;
+        BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+    }
+}
