a57c546c03f464b5a87ad785752136bec09a580b
Almar Mulder <almar.mulder@live.nl>
2022-07-13 19:50:05 +0200

    Added biome background images to background folder

--- World/SvartalvheimBiome.cs
+++ World/SvartalvheimBiome.cs
@@ -1,36 +1,35 @@
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using System;
 using Yggdrasil.Content.Players;
-using Yggdrasil.Extensions;
 
 namespace Yggdrasil.World;
 
 public class SvartalvheimBiome : ModBiome
 {
-    //public override ModUndergroundBackgroundStyle UndergroundBackgroundStyle => ModContent.Find<ModUndergroundBackgroundStyle>("Yggdrasil/SvartalvheimBiomeBackgroundStyle");
+    public override ModUndergroundBackgroundStyle UndergroundBackgroundStyle =>
+        ModContent.Find<ModUndergroundBackgroundStyle>("Yggdrasil/SvartalvheimBiomeBackgroundStyle");
 
     public override int Music => MusicID.OtherworldlyIce;
 
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
 
-    public override string BestiaryIcon => ("Yggdrasil/Assets/Textures/SvartalvheimBiome/SvartalvheimBiome_Icon");
-    public override string BackgroundPath => base.BackgroundPath;
-    public override Color? BackgroundColor => base.BackgroundColor;
+    public override string BestiaryIcon => "Yggdrasil/Assets/Textures/SvartalvheimBiome/Icons/SvartalvheimBiome_Icon";
+    // public override string BackgroundPath => base.BackgroundPath;
+    // public override Color? BackgroundColor => base.BackgroundColor;
 
     public override bool IsBiomeActive(Player player)
     {
-        var RunePlayer = Main.LocalPlayer.GetModPlayer<RunePlayer>();
+        var runePlayer = Main.LocalPlayer.GetModPlayer<RunePlayer>();
         if (YggdrasilWorld.SvartalvheimTiles > 7000)
-        {         
+        {
             //Main.NewText("In Svartalvheim");
-            RunePlayer.ZoneSvartalvheim = true;
+            runePlayer.ZoneSvartalvheim = true;
             return true;
         }
+
         //Main.NewText("NOT in Svartalvheim");
-        RunePlayer.ZoneSvartalvheim = false;
+        runePlayer.ZoneSvartalvheim = false;
         return false;
     }
 
--- World/SvartalvheimBiomeBackgroundStyle.cs
+++ World/SvartalvheimBiomeBackgroundStyle.cs
@@ -1,15 +1,18 @@
-/*using Terraria.ModLoader;
+using Terraria.ModLoader;
 
 namespace Yggdrasil.World
 {
-	public class SvartalvheimBiomeBackgroundStyle : ModUndergroundBackgroundStyle
-	{
-		public override void FillTextureArray(int[] textureSlots)
-		{
-			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot("Yggdrasil/Assets/Content/Textures/SvartalvheimBiome/SvartalvheimUnderground0");
-			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot("Yggdrasil/Assets/Content/Textures/SvartalvheimBiome/SvartalvheimUnderground1");
-			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("Yggdrasil/Assets/Content/Textures/SvartalvheimBiome/SvartalvheimUnderground2");
-			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("Yggdrasil/Assets/Content/Textures/SvartalvheimBiome/SvartalvheimUnderground3");
-		}
-	}
-}*/
+    public class SvartalvheimBiomeBackgroundStyle : ModUndergroundBackgroundStyle
+    {
+        private const string BiomeAssetPath =
+            "Assets/Content/Textures/SvartalvheimBiome/Backgrounds/SvartalvheimUnderground";
+
+        public override void FillTextureArray(int[] textureSlots)
+        {
+            textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot(Mod, $"{BiomeAssetPath}0");
+            textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot(Mod, $"{BiomeAssetPath}1");
+            textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot(Mod, $"{BiomeAssetPath}2");
+            textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot(Mod, $"{BiomeAssetPath}3");
+        }
+    }
+}
